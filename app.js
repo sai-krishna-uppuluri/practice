4cc1e83a566c647ae2973c941b1b4d446f5ef0bc
sai-krishna-uppuluri <saikrishna.uppuluri5@gmail.com>
2022-05-08 14:41:00 +0000

    Unhandled Promise Error

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const databasePath = path.join(__dirname, "cricketTeam.db");
+const app = express();
+app.use(express.json());
+
+let database = null;
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000");
+    });
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+app.get("/players/", async (request, response) => {
+  const getAllPlayers = `SELECT * FROM cricket_team;`;
+  const getAllPlayersDetails = await database.all(getAllPlayers);
+  response.send(
+    getAllPlayersDetails.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
+
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayer = `SELECT * FROM cricket_team WHERE player_id = ${playerId};`;
+  const playerDetail = await database.get(getPlayer);
+  response.send(convertDbObjectToResponseObject(playerDetail));
+});
+
+module.exports = app;
